29bd3cc58be1ef7e1e84bde87af9c441816c3df3
thuhinkumar <51832407+thuhinkumar@users.noreply.github.com>
2020-08-09 21:05:50 +0530

    Update new_youtube_API.py

--- new_youtube_API.py
+++ new_youtube_API.py
@@ -9,45 +9,49 @@ import google_auth_oauthlib.flow
 import googleapiclient.discovery
 import googleapiclient.errors
 
-scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
 
-def main():
+def youtube_search(keys):
+    #getting the secret file
+    script_path = os.path.abspath(__file__) 
+    path_list = script_path.split(os.sep)
+    script_directory = path_list[0:len(path_list)-1]
+    rel_path = "client_secret.json"
+    path = "/".join(script_directory) + "/" + rel_path
+    
+    scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
+    
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
 
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = "YOUR_CLIENT_SECRET_FILE.json"
-
+    client_secrets_file = path
     # Get credentials and create an API client
     flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
         client_secrets_file, scopes)
     credentials = flow.run_console()
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, credentials=credentials)
-        
-    request = youtube.search().list(
-        part="id,snippet",
-        maxResults=25,
-        q="surfing",
-        relevanceLanguage="en",
-        safeSearch="moderate",
-        type="video",
-        videoCaption="closedCaption"
-    )
-    response = request.execute()
-    print(response)
     videos = []
-    #create list of videos
-    for search_result in response.get('items', []):
-        videos.append(search_result['id'])
-    print(videos)
-    
+    for key in keys:
+        request = youtube.search().list(
+            part="id,snippet",
+            maxResults=25,
+            q=key,
+            relevanceLanguage="en",
+            safeSearch="moderate",
+            type="video",
+            videoCaption="closedCaption"
+            )
+        response = request.execute()
+        #print(response)
+        #add to list of videos
+        for item in response['items']:
+            videos.append(item['id']['videoId'])
+
     # place videos into file
     with open('vidoes.txt','w') as f:
         f.write('\n'.join(videos))
-    
-
-if __name__ == "__main__":
-    main()
+keys = ['apple','surfing'] #fill in search keys to test
+youtube_search(keys)
